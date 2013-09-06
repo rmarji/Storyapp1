60093604ddd73b60a625d86f6e7909c6a16a8694
rmarji <raed@jogeeks.com>
2013-09-06 22:19:46 +0300

    changed the constants to class scene
    I dont there should be a constants class

--- src/com/jogeeks/storynumone/objects/Scene.java
+++ src/com/jogeeks/storynumone/objects/Scene.java
@@ -4,4 +4,9 @@ public class Scene {
 
 	Paragraph par = new Paragraph();
 	
+	//Scene types
+	public static int READ_IT_TO_ME = 1;
+	public static int READ_IT_MYSELF = 2;
+	public static int AUTO_PLAY = 3;
+	
 }
