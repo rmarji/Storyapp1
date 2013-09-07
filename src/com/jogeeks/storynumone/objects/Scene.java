31900b5f05b0669f7e75ab381443bfbc290e06cc
rmarji <raed@jogeeks.com>
2013-09-07 11:49:11 +0300

    fixed the text span thing, maoved it back to singlescene activity... untill I can fix the dependancy

--- src/com/jogeeks/storynumone/objects/Scene.java
+++ src/com/jogeeks/storynumone/objects/Scene.java
@@ -2,11 +2,6 @@ package com.jogeeks.storynumone.objects;
 
 public class Scene {
 
-<<<<<<< HEAD
-	Paragraph par = new Paragraph();
-=======
->>>>>>> 95ae758745dd7e70d9038b91c5c4319b5969bba4
-
 	//Scene types
 	public static final int READ_IT_MYSELF = 1;
 	public static final int READ_IT_TO_ME = 2;
@@ -37,9 +32,4 @@ public class Scene {
 	}
 	
 	
-	
-<<<<<<< HEAD
-=======
-
->>>>>>> 95ae758745dd7e70d9038b91c5c4319b5969bba4
 }
